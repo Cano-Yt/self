91b48e5b3740d6db2d4b47f9b802b64ccad162de
Cano-Yt <79362316+Cano-Yt@users.noreply.github.com>
2021-04-09 22:12:42 +0300

    Create örnek.js

---
+++ komutlar/örnek.js
@@ -0,0 +1,13 @@
+const discord = require("discord.js-selfbot")
+
+exports.run = async(client, message, args) => {
+
+}
+
+exports.conf = {
+  aliases: [],
+};
+
+exports.help = {
+  name: "örnek", 
+};
